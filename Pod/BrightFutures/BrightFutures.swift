afcac6c396786c7980a461706ad6d6017146172d
Daniel Thorpe <danthorpe@me.com>
2015-04-22 22:34:47 +0100

    [YAP-3]: Adds async read BrightFutures API

--- Pod/BrightFutures/BrightFutures.swift
+++ Pod/BrightFutures/BrightFutures.swift
@@ -8,15 +8,45 @@ import BrightFutures
 
 extension YapDatabaseConnection {
 
+    public func asyncRead<Object where Object: Persistable>(key: String) -> Future<Object?> {
+        let promise = Promise<Object?>()
+        asyncRead(key, completion: promise.success)
+        return promise.future
+    }
+
+    public func asyncRead<Value where Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(key: String) -> Future<Value?> {
+        let promise = Promise<Value?>()
+        asyncRead(key, completion: promise.success)
+        return promise.future
+    }
+}
+
+extension YapDatabaseConnection {
+
+    public func asyncRead<Object where Object: Persistable>(keys: [String]) -> Future<[Object]> {
+        let promise = Promise<[Object]>()
+        asyncRead(keys, completion: promise.success)
+        return promise.future
+    }
+
+    public func asyncRead<Value where Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(keys: [String]) -> Future<[Value]> {
+        let promise = Promise<[Value]>()
+        asyncRead(keys, completion: promise.success)
+        return promise.future
+    }
+}
+
+extension YapDatabaseConnection {
+
     public func asyncWrite<Object where Object: NSCoding, Object: Persistable>(object: Object) -> Future<Object> {
         let promise = Promise<Object>()
-        asyncWrite(object) { promise.success($0) }
+        asyncWrite(object, completion: promise.success)
         return promise.future
     }
 
     public func asyncWrite<Value where Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(value: Value) -> Future<Value> {
         let promise = Promise<Value>()
-        asyncWrite(value) { promise.success($0) }
+        asyncWrite(value, completion: promise.success)
         return promise.future
     }
 }
@@ -25,13 +55,13 @@ extension YapDatabaseConnection {
 
     public func asyncWrite<Objects, Object where Objects: SequenceType, Objects.Generator.Element == Object, Object: NSCoding, Object: Persistable>(objects: Objects) -> Future<[Object]> {
         let promise = Promise<[Object]>()
-        asyncWrite(objects) { promise.success($0) }
+        asyncWrite(objects, completion: promise.success)
         return promise.future
     }
 
     public func asyncWrite<Values, Value where Values: SequenceType, Values.Generator.Element == Value, Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(values: Values) -> Future<[Value]> {
         let promise = Promise<[Value]>()
-        asyncWrite(values) { promise.success($0) }
+        asyncWrite(values, completion: promise.success)
         return promise.future
     }
 }
@@ -40,7 +70,7 @@ extension YapDatabaseConnection {
 
     public func asyncRemove<Item where Item: Persistable>(item: Item) -> Future<Void> {
         let promise = Promise<Void>()
-        asyncRemove(item) { promise.success($0) }
+        asyncRemove(item, completion: promise.success)
         return promise.future
     }
 }
@@ -49,23 +79,54 @@ extension YapDatabaseConnection {
 
     public func asyncRemove<Items where Items: SequenceType, Items.Generator.Element: Persistable>(items: Items) -> Future<Void> {
         let promise = Promise<Void>()
-        asyncRemove(items) { promise.success($0) }
+        asyncRemove(items, completion: promise.success)
         return promise.future
     }
 }
 
+// MARK: - YapDatabase
+
+extension YapDatabase {
+
+    public func asyncRead<Object where Object: Persistable>(key: String) -> Future<Object?> {
+        let promise = Promise<Object?>()
+        asyncRead(key, completion: promise.success)
+        return promise.future
+    }
+
+    public func asyncRead<Value where Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(key: String) -> Future<Value?> {
+        let promise = Promise<Value?>()
+        asyncRead(key, completion: promise.success)
+        return promise.future
+    }
+}
+
+extension YapDatabase {
+
+    public func asyncRead<Object where Object: Persistable>(keys: [String]) -> Future<[Object]> {
+        let promise = Promise<[Object]>()
+        asyncRead(keys, completion: promise.success)
+        return promise.future
+    }
+
+    public func asyncRead<Value where Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(keys: [String]) -> Future<[Value]> {
+        let promise = Promise<[Value]>()
+        asyncRead(keys, completion: promise.success)
+        return promise.future
+    }
+}
 
 extension YapDatabase {
 
     public func asyncWrite<Object where Object: NSCoding, Object: Persistable>(object: Object) -> Future<Object> {
         let promise = Promise<Object>()
-        asyncWrite(object) { promise.success($0) }
+        asyncWrite(object, completion: promise.success)
         return promise.future
     }
 
     public func asyncWrite<Value where Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(value: Value) -> Future<Value> {
         let promise = Promise<Value>()
-        asyncWrite(value) { promise.success($0) }
+        asyncWrite(value, completion: promise.success)
         return promise.future
     }
 }
@@ -74,13 +135,13 @@ extension YapDatabase {
 
     public func asyncWrite<Objects, Object where Objects: SequenceType, Objects.Generator.Element == Object, Object: NSCoding, Object: Persistable>(objects: Objects) -> Future<[Object]> {
         let promise = Promise<[Object]>()
-        asyncWrite(objects) { promise.success($0) }
+        asyncWrite(objects, completion: promise.success)
         return promise.future
     }
 
     public func asyncWrite<Values, Value where Values: SequenceType, Values.Generator.Element == Value, Value: Saveable, Value: Persistable, Value.ArchiverType.ValueType == Value>(values: Values) -> Future<[Value]> {
         let promise = Promise<[Value]>()
-        asyncWrite(values) { promise.success($0) }
+        asyncWrite(values, completion: promise.success)
         return promise.future
     }
 }
@@ -89,7 +150,7 @@ extension YapDatabase {
 
     public func asyncRemove<Item where Item: Persistable>(item: Item) -> Future<Void> {
         let promise = Promise<Void>()
-        asyncRemove(item) { promise.success($0) }
+        asyncRemove(item, completion: promise.success)
         return promise.future
     }
 }
@@ -98,7 +159,7 @@ extension YapDatabase {
 
     public func asyncRemove<Items where Items: SequenceType, Items.Generator.Element: Persistable>(items: Items) -> Future<Void> {
         let promise = Promise<Void>()
-        asyncRemove(items) { promise.success($0) }
+        asyncRemove(items, completion: promise.success)
         return promise.future
     }
 }
