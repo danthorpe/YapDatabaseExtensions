48fc9fb0bfa7b42ec841421dc88508c4f335a155
Daniel Thorpe <danthorpe@me.com>
2015-09-03 19:27:55 +0100

    [YDB-35]: Updates example pods.

--- examples/iOS/Pods/Target Support Files/Pods-YapDBExtensionsMobileTests-YapDatabaseExtensions/Pods-YapDBExtensionsMobileTests-YapDatabaseExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YapDBExtensionsMobileTests_YapDatabaseExtensions : NSObject
-@end
-@implementation PodsDummy_Pods_YapDBExtensionsMobileTests_YapDatabaseExtensions
-@end
--- examples/iOS/Pods/YapDatabase/YapDatabase/Utilities/YapDatabaseQuery.h
+++ examples/iOS/Pods/YapDatabase/YapDatabase/Utilities/YapDatabaseQuery.h
@@ -3,50 +3,62 @@
 
 /**
  * A YapDatabaseQuery is used to pass SQL style queries into various extension classes.
- * The query that you pass represents everything after the SELECT clause of a query.
+ * The query is generally a subset of a full SQL query, as the system can handle various details automatically.
+ * 
+ * For example:
+ *
+ * query = [YapDatabaseQuery queryWithFormat:@"WHERE department = ? AND salary >= ?", deptStr, @(minSalary)];
+ * [secondaryIndex enumerateKeysAndObjectsMatchingQuery:query
+ *                                           usingBlock:^(NSString *collection, NSString *key, id object, BOOL *stop){
+ *     ...
+ * }];
+ *
+ * YapDatabaseQuery supports the following types as query parameters:
+ * - NSNumber
+ * - NSDate    (automatically converted to double via timeIntervalSinceReferenceDate)
+ * - NSString
+ * - NSData
+ * - NSArray   (of any regular type above)
+ * 
+ * Array example:
+ * 
+ * NSArray *departments = [self engineeringDepartments];
+ * query = [YapDatabaseQuery queryWithFormat:@"WHERE title = ? AND department IN (?)", @"manager", departments];
 **/
 @interface YapDatabaseQuery : NSObject
 
+#pragma mark Standard Queries
+
 /**
- * A YapDatabaseQuery is everything after the SELECT clause of a query.
- * Methods that take YapDatabaseQuery parameters will prefix your query string similarly to:
+ * A "standard" YapDatabaseQuery is everything after the SELECT clause of a query.
+ * Thus they generally start with "WHERE ...".
  * 
- * fullQuery = @"SELECT rowid FROM 'database' " + [yapDatabaseQuery queryString];
- * 
- * Example 1:
- * 
- * query = [YapDatabaseQuery queryWithFormat:@"WHERE jid = ?", message.jid];
- * [secondaryIndex enumerateKeysAndObjectsMatchingQuery:query
- *                                           usingBlock:^(NSString *key, id object, BOOL *stop){
- *     ...
- * }];
- * 
- * Please note that you can ONLY pass objective-c objects as parameters.
+ * Please note that you can ONLY pass objects as parameters.
  * Primitive types such as int, float, double, etc are NOT supported.
  * You MUST wrap these using NSNumber.
- * 
- * Example 2:
- *
- * query = [YapDatabaseQuery queryWithFormat:@"WHERE department = ? AND salary >= ?", dept, @(minSalary)];
- * [secondaryIndex enumerateKeysAndObjectsMatchingQuery:query
- *                                           usingBlock:^(NSString *key, id object, BOOL *stop){
- *     ...
- * }];
 **/
 + (instancetype)queryWithFormat:(NSString *)format, ...;
 
 /**
- * Shim that allows YapDatabaseQuery to be used from Swift.
+ * Alternative initializer if you have a prepared va_list.
+ * 
+ * Swift note:
+ * You may prefer to use 'queryWithString:(NSString *)queryString parameters:(NSArray *)queryParameters' instead.
  *
- * Define the following somewhere in your Swift code:
+ * Alternatively, define the following somewhere in your Swift code:
  *
  * extension YapDatabaseQuery {
  *     class func queryWithFormat(format: String, _ arguments: CVarArgType...) -> YapDatabaseQuery? {
  *         return withVaList(arguments, { YapDatabaseQuery(format: format, arguments: $0) })
  *     }
  * }
- **/
+**/
 + (instancetype)queryWithFormat:(NSString *)format arguments:(va_list)arguments;
+
+/**
+ * Alternative initializer - generally preferred for Swift code.
+**/
++ (instancetype)queryWithString:(NSString *)queryString parameters:(NSArray *)queryParameters;
 
 /**
  * Shorthand for a query with no 'WHERE' clause.
@@ -54,7 +66,39 @@
 **/
 + (instancetype)queryMatchingAll;
 
-@property (nonatomic, strong, readonly) NSString *queryString;
-@property (nonatomic, strong, readonly) NSArray *queryParameters;
+#pragma mark Aggregate Queries
+
+/**
+ * Aggregate Queries (avg, max, min, sum, ...)
+ * 
+ * For example:
+ *
+ * // Figure out how much the "dev" department costs the business via salaries.
+ * // We do this by asking the database to sum the salary column(s) matching the given query.
+ *
+ * YapDatabaseQuery *query = [YapDatabaseQuery queryWithAggregateFunction:@"SUM(salary)"
+ *                                                                 format:@"WHERE department = ?", @"dev"];
+ *
+ * For more inforation, see the sqlite docs on "Aggregate Functions":
+ * https://www.sqlite.org/lang_aggfunc.html
+**/
++ (instancetype)queryWithAggregateFunction:(NSString *)aggregateFunction format:(NSString *)format, ...;
+
++ (instancetype)queryWithAggregateFunction:(NSString *)aggregateFunction
+                                    format:(NSString *)format
+                                 arguments:(va_list)arguments;
+
++ (instancetype)queryWithAggregateFunction:(NSString *)aggregateFunction
+                                    string:(NSString *)queryString
+                                parameters:(NSArray *)queryParameters;
+
+
+#pragma mark Properties
+
+@property (nonatomic, copy, readonly) NSString *aggregateFunction;
+@property (nonatomic, copy, readonly) NSString *queryString;
+@property (nonatomic, copy, readonly) NSArray *queryParameters;
+
+@property (nonatomic, readonly) BOOL isAggregateQuery;
 
 @end
