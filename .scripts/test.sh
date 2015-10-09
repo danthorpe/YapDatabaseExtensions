755d016affe80f92456270b5aade69d301ad5e13
Daniel Thorpe <danthorpe@me.com>
2015-10-09 22:50:32 +0100

    [YDB-44]: Forces branch into codecov script.

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -2,4 +2,4 @@
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
 bundle update && bundle exec fastlane test
-bash <(curl -s https://codecov.io/bash) -f .fastlane/xcodebuild-data/Build/Intermediates/CodeCoverage/YapDatabaseExtensions/Coverage.profdata
+bash <(curl -s https://codecov.io/bash) -f .fastlane/xcodebuild-data/Build/Intermediates/CodeCoverage/YapDatabaseExtensions/Coverage.profdata -B $(BUILDKITE_BRANCH)
