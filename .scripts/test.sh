c8e090372660c90b2e2ae854452f58a5f88bd64b
Daniel Thorpe <danthorpe@me.com>
2015-10-10 15:40:34 +0100

    [YDB-44]: Moving back to manual script.

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -2,4 +2,4 @@
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
 bundle update && bundle exec fastlane test
-bash <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/master/codecov)
+.scripts/codecov.bash
