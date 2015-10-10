ae5dc07341a4ba537fd71d0e42aec24337b52aec
Daniel Thorpe <danthorpe@me.com>
2015-10-10 14:49:21 +0100

    [YDB-44]: Using test codecov script...

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -2,4 +2,4 @@
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
 bundle update && bundle exec fastlane test
-.scripts/codecov.bash
+bash <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/master/codecov)
