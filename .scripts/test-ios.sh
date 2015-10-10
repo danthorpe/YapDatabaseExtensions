808e0ba4549d0e2b44c2cdba376d11e4839173a4
Daniel Thorpe <danthorpe@me.com>
2015-10-10 20:06:12 +0100

    [YDB-43]: Updates scripts to run on OS X.

--- .scripts/test-ios.sh
+++ .scripts/test-ios.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
-bundle update && bundle exec fastlane test
+bundle update && bundle exec fastlane test_ios
 bash <(curl -s https://codecov.io/bash) -D .fastlane/xcodebuild-data
