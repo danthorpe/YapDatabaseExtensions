53edbe444018ccc5c2e74747bbc1da5ad3cb5a57
Daniel Thorpe <danthorpe@me.com>
2015-10-10 20:08:55 +0100

    [YDB-43]: Fixes errors in scripts.

--- .scripts/test-ios.sh
+++ .scripts/test-ios.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
-bundle update && bundle exec fastlane test_ios
+bundle update && bundle exec fastlane ios test
 bash <(curl -s https://codecov.io/bash) -D .fastlane/xcodebuild-data
