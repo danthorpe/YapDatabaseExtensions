ca33733f6b638fabf9542dda46d861cdf7c52b52
Daniel Thorpe <danthorpe@me.com>
2015-10-10 16:20:41 +0100

    [YDB-44]: Uses codecov file.

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -2,4 +2,5 @@
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
 bundle update && bundle exec fastlane test
-.scripts/codecov.bash
+#.scripts/codecov.bash
+bash <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/master/codecov) -D .fastlane/xcodebuild-data
