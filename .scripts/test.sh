aae2cd950fbaf4f830b05c8142e1168b90f16c9d
Daniel Thorpe <danthorpe@me.com>
2015-10-10 16:40:30 +0100

    [YDB-44]: Updates script as its on production @codecov

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -2,5 +2,4 @@
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
 bundle update && bundle exec fastlane test
-#.scripts/codecov.bash
-bash <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/master/codecov) -D .fastlane/xcodebuild-data
+bash <(curl -s https://codecov.io/bash) -D .fastlane/xcodebuild-data
