5daf51d6b2c29645f7a7260b6ceeb43863c747c6
Daniel Thorpe <danthorpe@me.com>
2015-11-11 16:35:00 +0000

    [YDB-71]: Uses Carthage instead of CP for dependencies

--- Pods/Target Support Files/Pods-YapDatabaseExtensions-OSXTests-YapDatabase/Pods-YapDatabaseExtensions-OSXTests-YapDatabase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YapDatabaseExtensions_OSXTests_YapDatabase : NSObject
-@end
-@implementation PodsDummy_Pods_YapDatabaseExtensions_OSXTests_YapDatabase
-@end
