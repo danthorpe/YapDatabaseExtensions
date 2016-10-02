6c11b49d0345143312c608a26ebe15fedead7fcf
Daniel Thorpe <danthorpe@me.com>
2016-10-02 17:16:25 +0100

    YapDatabaseExtensions (#89)
    A single cross-platform framework. Updating to "the new way of doing things" :)
    
    * [YDB-89]: Re-creates Xcode project to be a cross-platform single framework
    
    * [YBD-89]: Updates CI scripts
    
    * [YDB-89]: Updates gem dependencies
    
    * [YDB-89]: Adds bundle install to the scripts
    
    * [YDB-89]: Updates Fastfile
    
    * [YDB-89]: Shares YapDatabaseExtensions scheme
    
    * [YDB-89]: Removes xcargs of derived data path
    
    * [YDB-89]: Fixes mac platform for scan
    
    * [YDB-89]: Adds correct device for iOS

---
+++ Supporting Files/YapDatabaseExtensions.h
@@ -0,0 +1,19 @@
+//
+//  YapDatabaseExtensions.h
+//  YapDatabaseExtensions
+//
+//  Created by Daniel Thorpe on 27/09/2016.
+//
+//
+
+@import Foundation;
+
+//! Project version number for YapDatabaseExtensions.
+FOUNDATION_EXPORT double YapDatabaseExtensionsVersionNumber;
+
+//! Project version string for YapDatabaseExtensions.
+FOUNDATION_EXPORT const unsigned char YapDatabaseExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YapDatabaseExtensions/PublicHeader.h>
+
+
